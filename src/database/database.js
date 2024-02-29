f14bb0df987cb43c43e73d54925940a0ef127efe
Ronald Mutebi <mutebironaldroninho@gmail.com>
2024-02-29 16:29:52 +0300

    add database connection Singleton

---
+++ src/database/database.js
@@ -0,0 +1,40 @@
+const { connect } = require("mongoose");
+
+const url = process.env.MONGO_URI;
+
+const connectDB = connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(
+    () => console.log("Databse connected-----"),
+    () => console.log("Error connecting to database----")
+  )
+  .catch((err) => {
+    console.log(err);
+    process.exit(1);
+  });
+
+class Database {
+  constructor() {
+    connectDB();
+  }
+
+  getInstanceStatus() {
+    return process.env.NODE_ENV === "production" ? "__production" : "__staging";
+  }
+}
+
+class Singleton {
+  constructor() {
+    if (!Singleton.instance) {
+      Singleton.instance = new Database();
+    }
+  }
+
+  getInstance() {
+    return Singleton.instance;
+  }
+}
+
+module.exports = Singleton;
