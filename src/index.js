7c89098fd5cb7f2ce83641b7f550699d2b6c715d
Ronald Mutebi <mutebironaldroninho@gmail.com>
2024-03-12 13:46:31 +0300

    remove unnecessary bodyparser

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-const bodyParser = require("body-parser");
 
 // require("dotenv").config();
 
@@ -11,10 +10,10 @@ const limiter = require("./middleware/rateLimitingMiddleware");
 const app = express();
 
 app.use(cors());
-app.use(bodyParser.json({}));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+app.use("/", api);
 app.use(limiter);
-// app.use("/api", api);
 
 const port = process.env.PORT;
 app.listen(port, () => {
