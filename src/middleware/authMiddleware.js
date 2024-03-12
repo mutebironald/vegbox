24c2b463f06d4f9ddad309a0291cde6fee7a0e05
Ronald Mutebi <mutebironaldroninho@gmail.com>
2024-03-12 13:45:17 +0300

    add auth middleware

---
+++ src/middleware/authMiddleware.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken");
+
+const authenticateUser = (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return res.status(401).json({ message: "Authorization token is required" });
+  }
+
+  try {
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decodedToken;
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
+};
+
+module.exports = authenticateUser;
