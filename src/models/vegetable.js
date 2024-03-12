0f0fb7bdf144500597f88a61d02b6d6dcbc31299
Ronald Mutebi <mutebironaldroninho@gmail.com>
2024-03-12 13:44:27 +0300

    enforce model requirements

--- src/models/vegetable.js
+++ src/models/vegetable.js
@@ -4,9 +4,9 @@ const { Schema, model } = require("mongoose");
  * could be extended to include nutritional information, category, etc
  */
 const vegetableSchema = new Schema({
-  name: String,
-  color: String,
-  price: String,
+  name: {type: String, required: true, unique: true },
+  color: { type: String, required: true },
+  price: { type: String, required: true},
 });
 
 module.exports = model("Vegetable", vegetableSchema);
