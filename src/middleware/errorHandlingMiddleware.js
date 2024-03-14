258c9ea0371f9d51bacc8d979c107a3fc9ddbd48
Ronald Mutebi <mutebironaldroninho@gmail.com>
2024-03-14 14:45:47 +0300

    pass error to next middleware

--- src/middleware/errorHandlingMiddleware.js
+++ src/middleware/errorHandlingMiddleware.js
@@ -1,6 +1,7 @@
 const errorHandlingMiddleware = (err, req, res, next) => {
   console.log(err.stack);
   res.status(500).json({ message: "Internal Server Error" });
+  next(err);
 };
 
 module.exports = errorHandlingMiddleware;
